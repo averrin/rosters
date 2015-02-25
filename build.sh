02537af83d3b7e3817ac58fadd761441ecd9ba71
alexey.nabrodov <alexey.nabrodov@dev.zodiac.tv>
2015-02-25 12:51:32 +0300

    some more order

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
- #!/bin/bash
- rm *.pdf
- for i in *.html
- do
-  if [ "$i" != "index.html" ]
-  then
-    gnome-web-print -m print --file "$i" "$i".pdf
-   fi
- done
- pdfunite *.pdf out
- rm *.pdf
- mv out rosters.pdf
+#!/bin/bash
+cd rosters
+rm *.pdf
+for i in *.html
+do
+if [ "$i" != "index.html" ]
+then
+gnome-web-print -m print --file "$i" "$i".pdf
+fi
+done
+pdfunite *.pdf out
+rm *.pdf
+mv out ../rosters.pdf
