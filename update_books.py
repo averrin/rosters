66bf5ff2f5de93d28aed70c579b4493de77f5f22
alexey.nabrodov <alexey.nabrodov@dev.zodiac.tv>
2015-03-03 19:21:46 +0300

    test new book_update

--- update_books.py
+++ update_books.py
@@ -5,8 +5,9 @@ import os, sys
 import io
 
 books = os.listdir('books')
-present = json.load(io.open("data/books.json", 'r', encoding='utf8'))
+present = json.load(io.open("data/books.json", 'r', encoding='utf8'), encoding='utf8')
 for b in books:
+	b = b.encode("utf8")
 	ishere = b in [p['filename'] for p in present]
 	if not ishere:
 		present.append({"filename": b, "name": b})
@@ -14,7 +15,9 @@ for b in books:
 for b in present[:]:
 	if b['filename'] not in books:
 		present.remove(b)
+		pass
 
+print(present)
 with io.open("data/books.json", 'w', encoding='utf8') as f:
-	dump = json.dumps(present, f, indent=4, ensure_ascii=False, encoding="utf-8").decode("utf8")
+	dump = json.dumps(present, indent=4, ensure_ascii=False, encoding="utf-8") #.decode("utf8")
 	f.write(dump)
